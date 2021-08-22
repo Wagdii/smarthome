94f9b681ba4702de8da1cca50e09e1d2beeb7731
Wagdii <abdallahwagdy70@yahoo.com>
2021-08-22 19:55:08 +0200

    First Commit

---
+++ HC05.h
@@ -0,0 +1,12 @@
+/*
+ * HC05.h
+ *
+  */ 
+
+
+#ifndef HC05_H_
+#define HC05_H_
+#include "UART.h"
+void HC05_Init(void);
+uint8_t HC05_Receive(void);
+#endif /* HC05_H_ */
---
+++ UART.h
@@ -0,0 +1,16 @@
+/*
+* UART.h
+*
+*/
+
+
+#ifndef UART_H_
+#define UART_H_
+
+#include "UART_CONFIG.h"
+
+void UART_Init(void);
+void UART_Transmit(uint8_t data);
+uint8_t UART_Receive(void);
+UART_Status UART_RECEIVE(uint8_t* data);
+#endif /* UART_H_ */
